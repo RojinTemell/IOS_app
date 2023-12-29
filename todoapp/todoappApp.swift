16aa4e2c606a875db3845fb8c65ccba908b675b0
rojintemel <rojin@bynogame.tech>
2023-12-29 16:18:19 +0300

    Initial Commit

---
+++ todoapp/todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappApp.swift
+//  todoapp
+//
+//  Created by rojin on 29.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
